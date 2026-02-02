349e3b4c550e027af0e107bffcd3fed0cdbc2db3
adrianreigfernandez-oss <reigfernandezadrian@gmail.com>
2026-02-02 11:55:42 +0100

    Improimo resultados

---
+++ SRC/download.py
@@ -0,0 +1,15 @@
+import requests
+
+from config import ACCESS_TOKEN
+
+url_prueba= "https://api.themoviedb.org/3/authentication"
+url_popular_movies = "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1"
+
+headers = {
+    "accept": "application/json",
+    "Authorization": f"Bearer {ACCESS_TOKEN} "
+}
+
+response = requests.get(url_popular_movies, headers=headers)
+
+print(response.json()["results"])
